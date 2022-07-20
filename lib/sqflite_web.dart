ad44d88af217cc83b5a0184358911dad7c8b0f00
Mohammed Alimoor <ameral.java@gmail.com>
2022-07-19 22:46:09 -0700

    Update sqflite_web.dart

--- lib/sqflite_web.dart
+++ lib/sqflite_web.dart
@@ -1,3 +1,3 @@
-export 'src/database_factory.dart' show DatabaseFactoryWeb;
+//export 'src/database_factory.dart' show DatabaseFactoryWeb;
 export 'src/sqflite_web_stub.dart';
 export 'src/sqflite_web.dart';
